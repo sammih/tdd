96573955b473338fc853bc74b837c1696b6a258c
sammih <samcessy@gmail.com>
2019-07-26 10:10:41 +0300

    replaced bootstrap css framework with tailwind css

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,21 +2,20 @@
 
 @section('content')
 
-  <div class="flex items-center">
-      <h1 style="margin-right: auto">BirdBoard</h1>
+  <div class="flex items-center mb-3">
       <a href="/projects/create">New projects</a>
   </div>
 
-  <ul>
+  <div class="flex">
     @forelse ($projects as $project)
+      <div class="bg-white mr-4 rounded p-5 shadow w-1/3">
+        <h3 class="foont-normal text-xl py-4">{{ $project->title }}</h3>
 
-      <li>
-        <a href="{{ $project->path() }}">{{ $project->title }}</a>
-      </li>
-
+        <div class="text-grey">{{ str_limit($project->description, 100) }}</div>
+      </div>
     @empty
-      <p>No projects yet</p>    
+      <div>No projects yet</div>    
     @endforelse
-  </ul>
+  </div>
 
 @endsection
